61c1342515aace816e264ae78712bcebe41d2b94
jamie dixon <jamessdixon@gmail.com>
2019-11-12 14:46:36 -0500

    Initial Commit

---
+++ src/MyFunctionApp.FS/Function2.fs
@@ -0,0 +1,10 @@
+﻿namespace MyFunctionApp.FS
+
+open System
+open Microsoft.Azure.WebJobs
+open Microsoft.Extensions.Logging
+
+module Function2 =
+    [<FunctionName("Function2")>]
+    let run ([<TimerTrigger("0 */5 * * * *")>]myTimer:TimerInfo, log:ILogger) =
+        log.LogInformation("F# Timer trigger function executed at:" + DateTime.Now.ToString())
